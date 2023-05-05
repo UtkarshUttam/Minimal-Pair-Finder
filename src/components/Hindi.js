a70ca93032be305ba4aee34ba49def4e68b6a083
Utkarsh Uttam <utkarshuttam2044@gmail.com>
2023-05-05 14:20:08 +0530

    Create Hindi.js

---
+++ src/components/Hindi.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import MinimalPairFinder from './MinimalPairFinder'
+
+
+export default class Hindi extends Component {
+  render() {
+    return (
+      <div>
+        <MinimalPairFinder language="Hindi"/>
+      </div>
+    )
+  }
+}
