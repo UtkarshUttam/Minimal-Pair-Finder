c01daf3a4a276ec928e7128f54f4baddbe4dcf49
Utkarsh Uttam <utkarshuttam2044@gmail.com>
2023-05-05 14:20:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
+import Hindi from './components/Hindi'
+import Gujarati from './components/Gujarati'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 // import TextEntry from './components/TextEntry'
-import MinimalPairChecker from './components/MinimalPairChecker'
+// import MinimalPairChecker from './components/MinimalPairChecker'
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <Navbar/>
-        {/* <TextEntry/> */}
-        <MinimalPairChecker></MinimalPairChecker>
+        <Routes>
+          <Route path="/" element={<Hindi/>}></Route>
+          <Route path="/Gujarati" element={<Gujarati/>}></Route>        
+        {/* <MinimalPairChecker></MinimalPairChecker> */}
+        </Routes>
+        </Router>
       </div>
     )
   }
