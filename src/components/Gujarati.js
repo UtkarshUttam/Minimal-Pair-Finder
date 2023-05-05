32b8994950dc65f396512d06c1cefea76a7f5173
Utkarsh Uttam <utkarshuttam2044@gmail.com>
2023-05-05 14:20:12 +0530

    Create Gujarati.js

---
+++ src/components/Gujarati.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import MinimalPairFinder from './MinimalPairFinder'
+
+export class Gujarati extends Component {
+  render() {
+    return (
+        <div>
+            <MinimalPairFinder language="Gujarati"/>
+        </div>
+    )
+  }
+}
+
+export default Gujarati
