391642e583ff55d57a89c6c05c9f68f367cfb0d0
Utkarsh Uttam <utkarshuttam2044@gmail.com>
2023-05-15 14:05:10 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,19 @@ export default class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark"  data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">MPF</a>
+    <a className="navbar-brand" href="/">
+      <img src="https://vitbhopal.ac.in/file/2020/01/logochhota.png" alt="VitB" width="170" height="70"/>
+    </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Hindi</Link>
+      <ul className="navbar-nav me-auto mb-2 mb-1 g-1">
+        <li className="nav-item mx-4">
+          <Link className="nav-link fs-4" aria-current="page" to="/">Hindi</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/Gujarati">Gujarati</Link>
+          <Link className="nav-link fs-4" aria-current="page" to="/Gujarati">Gujarati</Link>
         </li>
         {/* <li className="nav-item">
           <a className="nav-link" href="#">Link</a>
