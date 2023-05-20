13eacee2beb571ef2b70fd2f9c2e76c0e4c26778
Utkarsh Uttam <utkarshuttam2044@gmail.com>
2023-05-20 09:30:38 +0530

    Create Bengali.js

---
+++ src/components/Bengali.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import BengaliMininalPairFinder from './Bengali_MinimalPairFinder'
+
+export default function Bengali() {
+  return (
+    <div>
+        <BengaliMininalPairFinder/>
+    </div>
+  )
+}
